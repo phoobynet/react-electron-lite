a7615f1d08fab7ac54fc7b579474fc44bcc2a264
Glenn <phoobynet@users.noreply.github.com>
2022-08-12 13:14:27 +0100

    wip

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="flex min-h-screen min-w-full justify-center">
-      <div className="mt-[150px] h-[500px] w-1/2 rounded-lg border border-gray-300 p-6 shadow-lg">
+    <div className="flex min-w-full justify-center">
+      <div className="mt-[150px] w-1/2 rounded-lg border border-gray-300 p-6 shadow-lg">
         <div className="text-5xl font-bold">Welcome!</div>
         <h5 className="mt-5 text-3xl font-bold">What is this?</h5>
         <p className="mt-3">
@@ -13,10 +13,12 @@ export default function Home() {
           The electron part sits in the{' '}
           <code className="text-blue-500">/main</code> directory. However, the
           electron part is just <strong>JavaScript</strong> to simplify and
-          speed up the build configuration.
+          speed up the build configuration. Otherwise, the renderer or frontend
+          in the <code className="text-blue-500">/src</code> folder is just{' '}
+          <strong>React</strong> and <strong>TypeScript</strong>.
         </p>
 
-        <p className="mt-3">
+        <p className="mt-4 inline-block bg-yellow-200">
           If you just want to put a web app on the desktop, this could be one
           way of doing it.
         </p>
